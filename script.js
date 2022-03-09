1890ef0994889625578c816ec7ae0db21138e2b1
heitorgames1237 <59296528+heitorgames1237@users.noreply.github.com>
2022-03-09 17:58:00 -0300

    some Updates

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ function calculate() {
   var height = (document.getElementById("height").value)/100
   var weight = document.getElementById("weight").value
 
+  if(height == 0 && weight ==0 || height == 0 || weight == 0) {
+      window.alert("Informações inválidas");
+  }
+
   var imc=weight / height ** 2;
   if(imc<18.5){
-    window.alert("Você está abaixo do peso ideal")
+      window.alert("Você está abaixo do peso ideal")
   }else if(imc<24.9){
-    window.alert("Você está no peso ideal")
+      window.alert("Você está no peso ideal")
   }else if(imc<29.9){
-    window.alert("Você está com sobre-peso")
+      window.alert("Você está com sobre-peso")
   }else if(imc<39.9){
-    window.alert("Você está com obesidade")
+      window.alert("Você está com obesidade")
   }else if(imc>39.9){
-    window.alert("Você está com obesidade mórbida")
-  }
+      window.alert("Você está com obesidade mórbida")
+  };
 }
